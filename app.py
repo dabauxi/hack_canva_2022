1436c1452dfe094826b77f822e2fbb2689cb1176
Paul Angerer <dabauxi@users.noreply.github.com>
2022-07-21 08:49:34 +0200

    Clean up code

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# import the necessary packages
 from flask import Flask, Response, render_template
 
 from camera import VideoCamera
@@ -8,13 +7,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # rendering webpage
     return render_template("index.html")
 
 
 def gen(camera):
     while True:
-        # get camera frame
         frame = camera.get_frame()
         yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
 
@@ -27,5 +24,4 @@ def video_feed():
 
 
 if __name__ == "__main__":
-    # defining server ip address and port
     app.run(host="0.0.0.0", port="5000", debug=True)
--- camera.py
+++ camera.py
@@ -1,7 +1,6 @@
 import cv2
 
-# face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
-ds_factor = 0.6
+DS_FACTOR = 0.6
 
 
 class VideoCamera(object):
@@ -14,12 +13,7 @@ class VideoCamera(object):
     def get_frame(self):
         success, image = self.video.read()
         image = cv2.resize(
-            image, None, fx=ds_factor, fy=ds_factor, interpolation=cv2.INTER_AREA
+            image, None, fx=DS_FACTOR, fy=DS_FACTOR, interpolation=cv2.INTER_AREA
         )
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # face_rects = face_cascade.detectMultiScale(gray, 1.3, 5)
-        # for (x, y, w, h) in face_rects:
-        #     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        #     break
         ret, jpeg = cv2.imencode(".jpg", image)
         return jpeg.tobytes()
